a9379a5718f45c644b2421adacdc4abe9341cd63
Mr Robot <qomc16@gmail.com>
2021-03-08 19:37:33 -0600

    Versión 1 UwU

--- src/main/java/com/mycompany/calcu_parcial/Main.java
+++ src/main/java/com/mycompany/calcu_parcial/Main.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.calcu_parcial;
-
-/**
- *
- * @author familia
- */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
+ //@author Fsociety
+ public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+    Pantalla p=new Pantalla();
+    p.setVisible(true);
     }
     
 }
