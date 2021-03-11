b48ff6fb15def1d19ff0aa72071fef753213086e
oscar <oscar@oscar-OptiPlex-3010>
2021-03-11 12:32:53 -0600

    Versión Beta

--- src/main/java/com/mycompany/calcu_parcial/Pantalla.java
+++ src/main/java/com/mycompany/calcu_parcial/Pantalla.java
@@ -69,6 +69,7 @@ public class Pantalla extends javax.swing.JFrame {
         jLabel9 = new javax.swing.JLabel();
         jButton30 = new javax.swing.JButton();
         jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -83,7 +84,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton23);
-        jButton23.setBounds(20, 90, 60, 25);
+        jButton23.setBounds(20, 90, 80, 23);
 
         Ingreso.setEditable(false);
         Ingreso.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
@@ -97,7 +98,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(90, 130, 50, 25);
+        jButton1.setBounds(70, 130, 60, 23);
 
         jButton2.setText("^1/2");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -106,7 +107,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton2);
-        jButton2.setBounds(140, 130, 60, 25);
+        jButton2.setBounds(130, 130, 70, 23);
 
         jButton3.setText("CE");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
@@ -115,7 +116,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton3);
-        jButton3.setBounds(200, 130, 50, 25);
+        jButton3.setBounds(200, 130, 60, 23);
 
         jButton4.setText("C");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -124,7 +125,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton4);
-        jButton4.setBounds(250, 130, 50, 25);
+        jButton4.setBounds(260, 130, 60, 23);
 
         jButton5.setText("2");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -133,7 +134,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton5);
-        jButton5.setBounds(110, 320, 50, 25);
+        jButton5.setBounds(110, 320, 50, 23);
 
         jButton6.setText("1");
         jButton6.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -143,7 +144,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton6);
-        jButton6.setBounds(60, 320, 50, 25);
+        jButton6.setBounds(60, 320, 50, 23);
 
         jButton7.setText("3");
         jButton7.addActionListener(new java.awt.event.ActionListener() {
@@ -152,7 +153,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton7);
-        jButton7.setBounds(160, 320, 50, 25);
+        jButton7.setBounds(160, 320, 50, 23);
 
         jButton8.setText("+");
         jButton8.addActionListener(new java.awt.event.ActionListener() {
@@ -161,7 +162,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton8);
-        jButton8.setBounds(210, 320, 50, 25);
+        jButton8.setBounds(210, 320, 50, 23);
 
         jButton9.setText("-");
         jButton9.addActionListener(new java.awt.event.ActionListener() {
@@ -170,7 +171,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton9);
-        jButton9.setBounds(260, 320, 50, 25);
+        jButton9.setBounds(260, 320, 50, 23);
 
         jButton10.setText("5");
         jButton10.addActionListener(new java.awt.event.ActionListener() {
@@ -179,7 +180,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton10);
-        jButton10.setBounds(110, 370, 50, 25);
+        jButton10.setBounds(110, 370, 50, 23);
 
         jButton11.setText("4");
         jButton11.addActionListener(new java.awt.event.ActionListener() {
@@ -188,7 +189,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton11);
-        jButton11.setBounds(60, 370, 50, 25);
+        jButton11.setBounds(60, 370, 50, 23);
 
         jButton12.setText("6");
         jButton12.addActionListener(new java.awt.event.ActionListener() {
@@ -197,7 +198,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton12);
-        jButton12.setBounds(160, 370, 50, 25);
+        jButton12.setBounds(160, 370, 50, 23);
 
         jButton13.setText("*");
         jButton13.addActionListener(new java.awt.event.ActionListener() {
@@ -206,7 +207,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton13);
-        jButton13.setBounds(210, 370, 50, 25);
+        jButton13.setBounds(210, 370, 50, 23);
 
         jButton14.setText("/");
         jButton14.addActionListener(new java.awt.event.ActionListener() {
@@ -215,7 +216,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton14);
-        jButton14.setBounds(260, 370, 50, 25);
+        jButton14.setBounds(260, 370, 50, 23);
 
         jButton15.setText("8");
         jButton15.addActionListener(new java.awt.event.ActionListener() {
@@ -224,7 +225,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton15);
-        jButton15.setBounds(110, 410, 50, 25);
+        jButton15.setBounds(110, 410, 50, 23);
 
         jButton16.setText("7");
         jButton16.addActionListener(new java.awt.event.ActionListener() {
@@ -233,7 +234,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton16);
-        jButton16.setBounds(60, 410, 50, 25);
+        jButton16.setBounds(60, 410, 50, 23);
 
         jButton17.setText("9");
         jButton17.addActionListener(new java.awt.event.ActionListener() {
@@ -242,7 +243,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton17);
-        jButton17.setBounds(160, 410, 50, 25);
+        jButton17.setBounds(160, 410, 50, 23);
 
         jButton18.setText("0");
         jButton18.addActionListener(new java.awt.event.ActionListener() {
@@ -251,7 +252,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton18);
-        jButton18.setBounds(210, 410, 50, 25);
+        jButton18.setBounds(210, 410, 50, 23);
 
         jButton19.setText(".");
         jButton19.addActionListener(new java.awt.event.ActionListener() {
@@ -260,7 +261,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton19);
-        jButton19.setBounds(260, 410, 50, 25);
+        jButton19.setBounds(260, 410, 50, 23);
 
         jButton20.setText("+/-");
         jButton20.addActionListener(new java.awt.event.ActionListener() {
@@ -269,7 +270,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton20);
-        jButton20.setBounds(170, 500, 60, 25);
+        jButton20.setBounds(170, 500, 60, 23);
 
         jButton21.setText("=");
         jButton21.addActionListener(new java.awt.event.ActionListener() {
@@ -278,7 +279,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton21);
-        jButton21.setBounds(230, 500, 79, 25);
+        jButton21.setBounds(230, 500, 79, 23);
 
         jButton22.setText("x⁻¹");
         jButton22.addActionListener(new java.awt.event.ActionListener() {
@@ -287,7 +288,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton22);
-        jButton22.setBounds(20, 200, 64, 25);
+        jButton22.setBounds(20, 200, 64, 23);
 
         jLabel1.setText("tan⁻¹");
         getContentPane().add(jLabel1);
@@ -300,11 +301,11 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton24);
-        jButton24.setBounds(110, 500, 64, 25);
+        jButton24.setBounds(84, 500, 90, 23);
 
         jLabel2.setText("^1/3");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(160, 110, 30, 15);
+        jLabel2.setBounds(160, 105, 30, 30);
 
         jLabel3.setText("π");
         getContentPane().add(jLabel3);
@@ -317,7 +318,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton25);
-        jButton25.setBounds(80, 200, 60, 25);
+        jButton25.setBounds(80, 200, 60, 23);
 
         jLabel4.setText("e");
         getContentPane().add(jLabel4);
@@ -325,7 +326,7 @@ public class Pantalla extends javax.swing.JFrame {
 
         jLabel5.setText("^");
         getContentPane().add(jLabel5);
-        jLabel5.setBounds(110, 110, 34, 15);
+        jLabel5.setBounds(100, 120, 34, 15);
 
         jButton26.setText("sin");
         jButton26.addActionListener(new java.awt.event.ActionListener() {
@@ -334,7 +335,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton26);
-        jButton26.setBounds(140, 200, 60, 25);
+        jButton26.setBounds(140, 200, 60, 23);
 
         jLabel6.setText("x!");
         getContentPane().add(jLabel6);
@@ -347,11 +348,11 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton27);
-        jButton27.setBounds(200, 200, 60, 25);
+        jButton27.setBounds(200, 200, 60, 23);
 
         jLabel7.setText("sin⁻¹");
         getContentPane().add(jLabel7);
-        jLabel7.setBounds(160, 180, 30, 15);
+        jLabel7.setBounds(150, 180, 50, 15);
 
         jButton28.setText("tan");
         jButton28.addActionListener(new java.awt.event.ActionListener() {
@@ -360,7 +361,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton28);
-        jButton28.setBounds(260, 200, 60, 25);
+        jButton28.setBounds(260, 200, 60, 23);
 
         jLabel8.setText("cos⁻¹");
         getContentPane().add(jLabel8);
@@ -373,7 +374,7 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton29);
-        jButton29.setBounds(10, 250, 70, 25);
+        jButton29.setBounds(10, 250, 70, 23);
 
         jLabel9.setText("Π");
         getContentPane().add(jLabel9);
@@ -386,11 +387,15 @@ public class Pantalla extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton30);
-        jButton30.setBounds(80, 250, 100, 25);
+        jButton30.setBounds(80, 250, 120, 23);
 
-        jLabel10.setText("nPr");
+        jLabel10.setText("nCr");
         getContentPane().add(jLabel10);
-        jLabel10.setBounds(220, 350, 40, 15);
+        jLabel10.setBounds(270, 350, 40, 15);
+
+        jLabel11.setText("nPr");
+        getContentPane().add(jLabel11);
+        jLabel11.setBounds(220, 350, 40, 15);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -472,15 +477,20 @@ public class Pantalla extends javax.swing.JFrame {
     private void jButton13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton13ActionPerformed
         //Boton * y nPr
         if (shift==1) {
-         seleccionarOperacion('p');  
+         seleccionarOperacion('P');  
         }else{
         seleccionarOperacion('*');
         }
     }//GEN-LAST:event_jButton13ActionPerformed
 
     private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
-        //Boton /
+        
+         //Boton / y nCr
+        if (shift==1) {
+         seleccionarOperacion('C');  
+        }else{
         seleccionarOperacion('/');
+        }
     }//GEN-LAST:event_jButton14ActionPerformed
 
     private void jButton19ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton19ActionPerformed
@@ -537,12 +547,45 @@ public class Pantalla extends javax.swing.JFrame {
                numero2*=numero2;
                numero2=numero1+numero2;
                numero2=Math.sqrt(numero2);
-               
                 break;
 
             case 'P':
-                
+            numero2=numero1-numero2;
+            
+            Recursividad recursividad = new Recursividad();
+            int factorial = recursividad.Factorial((int) numero1);
+                System.out.println(factorial);
+            
+         
+            Recursividad recursividad2 = new Recursividad();
+            int factorial2 = recursividad2.Factorial((int) numero2);
+                System.out.println(factorial2);
+            
+            
+            numero2=factorial/factorial2;
+                      
                 break;
+            
+            case 'C':
+            double numero3=numero2;
+            numero2=numero1-numero2;
+            
+            Recursividad recursividad3 = new Recursividad();
+            int factorial3 = recursividad3.Factorial((int) numero1);
+                System.out.println(factorial3);
+            
+         
+            Recursividad recursividad4 = new Recursividad();
+            int factorial4 = recursividad4.Factorial((int) numero2);
+                System.out.println(factorial4);
+            
+            Recursividad recursividad5= new Recursividad();
+            int factorial5 = recursividad5.Factorial((int) numero3);
+                System.out.println(factorial5);
+            
+            numero2=factorial3/(factorial4*factorial5);    
+                break;
+                
         }
         Ingreso.setText(String.valueOf(numero2));
     }//GEN-LAST:event_jButton21ActionPerformed
@@ -721,6 +764,7 @@ public class Pantalla extends javax.swing.JFrame {
     private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
